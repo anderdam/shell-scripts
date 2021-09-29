6206f570d4a6f7ce48035206076454493d4f0d46
anderdam <anderdam@gmail.com>
2021-09-29 13:52:42 -0300

    Shell scripts

---
+++ .history/localUser_20210929134922.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Display de UID and username of the user executing the script.
+# Display if the user is the root or not
+
+# Display the UID.
+
+# Display the username.
+
+# Display if the user is the root or not.
